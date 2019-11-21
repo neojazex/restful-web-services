f7b8a55999ce6356964bae3c95ba036f5136fa43
jason <jason@vintrace.com>
2019-11-22 07:51:25 +1100

    14. Step 03 - Creating a Hello World Service

---
+++ src/main/java/com/jhdemo/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.jhdemo.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+
+    //GET
+    //URI - /hello-world
+    //method = "Hello World"
+
+    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+    @GetMapping(path="/hello-world")
+    public String helloWorld(){
+        return "Hello World!!!";
+    }
+
+
+}
